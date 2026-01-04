bfc1cdc47b0b615ac96ba7c970955f46e6e44f02
anshuyas <anshuyasayaju17@gmail.com>
2026-01-04 21:22:32 +0545

    updated user repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,11 +1,12 @@
 import { IUser, UserModel } from "../models/user.model";
 
 export class UserRepository {
+  
   async createUser(user: Partial<IUser>) {
     return UserModel.create(user);
   }
 
   async findUserByEmail(email: string) {
-    return UserModel.findOne({ email });
+    return UserModel.findOne({ email }).select('+password'); // always select password
   }
 }
