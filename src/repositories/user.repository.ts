3311f5d517c3ef76bbc0a0188d67e4c7b01ddde7
anshuyas <anshuyasayaju17@gmail.com>
2026-01-04 19:01:38 +0545

    added user repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,47 +1,11 @@
 import { IUser, UserModel } from "../models/user.model";
 
-export interface IUserRepository {
-    getUserByUsername(username: string): Promise<IUser | null>;
-    getUserByEmail(email: string): Promise<IUser | null>;
-    // additional 
-    createUser(data: Partial<IUser>): Promise<IUser>;
-    getUserById(id: string): Promise<IUser | null>; // get one
-    getAllUsers(): Promise<IUser[]>; // get all 
-    updateOneUser(id: string, data: Partial<IUser>): Promise<IUser | null>; // update one
-    deleteOneUser(id: string): Promise<boolean | null>; // delete one 
-}
-// UserModel -> db.users collection
-export class UserRepository implements IUserRepository {
-    async createUser(data: Partial<IUser>): Promise<IUser> {
-        const user = new UserModel(data); // MongoDb Model
-        return await user.save();
-    }
-    async getUserByEmail(email: string): Promise<IUser | null> {
-        const user = await UserModel.findOne({ "email": email });
-        return user;
-    }
-    async getUserByUsername(username: string): Promise<IUser | null> {
-        const user = await UserModel.findOne({ "username": username });
-        return user;
-    }
+export class UserRepository {
+  async createUser(user: Partial<IUser>) {
+    return UserModel.create(user);
+  }
 
-    async getUserById(id: string): Promise<IUser | null> {
-        // UserModel.findOne({"_id": id})
-        const user = await UserModel.findById(id);
-        return user;
-    }
-    async getAllUsers(): Promise<IUser[]> {
-        const users = await UserModel.find();
-        return users;
-    }
-    async updateOneUser(id: string, data: Partial<IUser>): Promise<IUser | null> {
-        // UserModel.updateOne({"_id": id}, {$set: data})
-        const updatedUser = await UserModel.findByIdAndUpdate(id, data, { new: true });
-        return updatedUser;
-    }
-    async deleteOneUser(id: string): Promise<boolean | null> {
-        // UserModel.deleteOne({"_id": id})
-        const result = await UserModel.findByIdAndDelete(id);
-        return result ? true : null;
-    }
+  async findUserByEmail(email: string) {
+    return UserModel.findOne({ email });
+  }
 }
