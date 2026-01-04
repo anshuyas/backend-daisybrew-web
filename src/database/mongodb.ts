3b0c30e47b268a70c79957ccba953168dba61bc1
anshuyas <anshuyasayaju17@gmail.com>
2026-01-04 19:12:12 +0545

    added mongodb

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "../config";
 
-export async function connectDatabase() {
+export async function connectDatabase(){
     try {
         await mongoose.connect(MONGODB_URI);
-        console.log("Database connected successfully");
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Database Error:", error);
-        process.exit(1); // exit application on exception
+        process.exit(1); 
     }
 }
