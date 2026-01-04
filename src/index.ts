62a55933ce56899bf818813988695302337dcb25
anshuyas <anshuyasayaju17@gmail.com>
2026-01-04 21:17:41 +0545

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import express from "express";
 import { connectDatabase } from "./database/mongodb";
+import authRoutes from "./routes/auth.route";
 
 const app = express();
-
 app.use(express.json());
-
-connectDatabase();
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5050;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDatabase();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+}
+
+startServer();
